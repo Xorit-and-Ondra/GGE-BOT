e5bc65544b4e4b3e7c3e370696a2f55cc2adb92d
darrenthebozz <darrenthebozz1@gmail.com>
2025-12-29 13:07:37 +0000

    Initialize Git repository if not exists

--- start.bat
+++ start.bat
@@ -1,4 +1,12 @@
 @echo off
+
+if not exist ".git"\ (
+  git init -b main
+  git remote add origin https://github.com/darrenthebozz/GGE-BOT.git
+  git add .
+  git pull origin main
+)
+
 git pull --recurse-submodules
 call npm i
 start node main.js
