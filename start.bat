dac9e688c5dd3462ef335454e61ab9c895426c8e
darrenthebozz <dazwallcroft123@gmail.com>
2025-12-29 13:39:31 +0000

    Help those who download as zip

--- start.bat
+++ start.bat
@@ -1,13 +1,18 @@
 @echo off
 
 if not exist ".git"\ (
-  git init -b main
+  git init -b main 
   git remote add origin https://github.com/darrenthebozz/GGE-BOT.git
   git add .
+  git fetch origin
+  git reset --hard 
+  git clean -f -d
   git pull origin main
+  git submodule deinit -f plugins-extra
+  git submodule init plugins-extra
 )
 
-git pull --recurse-submodules
+git pull origin main --recurse-submodules
 call npm i
 start node main.js
 start http://127.0.0.1:3001
