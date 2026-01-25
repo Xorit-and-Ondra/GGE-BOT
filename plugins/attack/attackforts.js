4e25aadf07cfac3b7c5adbbe434eb73ea4f3ad93
darrenthebozz <darrenthebozz1@gmail.com>
2026-01-25 21:48:03 +0000

    Add back fort options

--- plugins/attack/attackforts.js
+++ plugins/attack/attackforts.js
@@ -5,48 +5,33 @@ if (isMainThread)
     return module.exports = {
         name: name,
         pluginOptions: [
+
+            { type: "Label", label: "Easy Forts", md: 2 },
+            { type: "Checkbox", label: "Attack Level 60", key: "allowLvl60Easy", default: true },
+            { type: "Checkbox", label: "Attack Level 70", key: "allowLvl70Easy", default: true },
+            { type: "Checkbox", label: "Attack Level 80", key: "allowLvl80Easy", default: true },
+            { type: "", md: 3},
+            
+            { type: "Label", label: "Hard Forts", md: 2 },
+            { type: "Checkbox", label: "Attack Level 40", key: "allowLvl40", default: false },
+            { type: "Checkbox", label: "Attack Level 50", key: "allowLvl50", default: false },
+            { type: "Checkbox", label: "Attack Level 60", key: "allowLvl60", default: false },
+            { type: "Checkbox", label: "Attack Level 70", key: "allowLvl70", default: false },
+            { type: "Checkbox", label: "Attack Level 80", key: "allowLvl80", default: false },
+
+            { type: "Label", label: "Other" },
+            { type: "Checkbox", label: "Buy Coins", key: "buycoins", default: true },
+            { type: "Checkbox", label: "Buy Deco", key: "buydeco", default: false },
+            { type: "Checkbox", label: "Buy XP", key: "buyxp", default: false },
+            { type: "Checkbox", label: "Use Coin", key: "useCoin", default: false },
             {
                 type: "Text",
                 label: "Com White List",
                 key: "commanderWhiteList"
-            },
-            {
-                type: "Checkbox",
-                label: "Easy forts only",
-                key: "easyfortsonly",
-                default: false
-            },
-            {
-                type: "Checkbox",
-                label: "Add worser forts",
-                key: "addworserforts",
-                default: false
-            },
-            {
-                type: "Checkbox",
-                label: "Use Coin",
-                key: "useCoin",
-                default: false
-            },
-            {
-                type: "Checkbox",
-                label: "Buy Coins",
-                key: "buycoins",
-                default: false
-            },
-            {
-                type: "Checkbox",
-                label: "Buy Deco",
-                key: "buydeco",
-                default: false
-            },
-            {
-                type: "Checkbox",
-                label: "Buy XP",
-                key: "buyxp",
-                default: false
-            }]
+            }
+        ]
     }
+
 
 const { getCommanderStats } = require("../../getEquipment")
 const { Types, getResourceCastleList, ClientCommands, areaInfoLock, AreaType, KingdomID } = require('../../protocols')
@@ -112,7 +97,7 @@ events.once("load", async () => {
     if (pluginOptions["allowLvl60Easy"]) allowedLevels.push(7)
     if (pluginOptions["allowLvl70Easy"]) allowedLevels.push(8)
     if (pluginOptions["allowLvl80Easy"]) allowedLevels.push(9)
-        
+
     if (allowedLevels.length === 0) allowedLevels = [7, 8, 9, 13, 14]
 
     const sourceCastleArea = (await getResourceCastleList()).castles.find(e => e.kingdomID == kid)
